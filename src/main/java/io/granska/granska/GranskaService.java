27848c755e253c7fa3240b292495e98a0f0ea723
davstromb <davstromb@gmail.com>
2017-03-10 18:19:08 +0100

    granska

--- src/main/java/io/granska/granska/GranskaService.java
+++ src/main/java/io/granska/granska/GranskaService.java
@@ -1,8 +1,5 @@
 package io.granska.granska;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import io.granska.entity.Analysis;
 import io.granska.granska.fakeadapter.FakeAdapter;
 import org.springframework.stereotype.Service;
@@ -20,14 +17,6 @@ public class GranskaService {
 
     public Analysis scrutinize(List<String> tokens) {
         System.out.println("About to run adapter");
-        final Analysis analysis = adapter.get(tokens);
-
-        try {
-            System.out.println(new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).writeValueAsString(analysis));
-
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return analysis;
+        return adapter.get(tokens);
     }
 }
